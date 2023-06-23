99b64362cb64902c2784f744c10434ede17d0ef9
DimasCW <cdimas292@gmail.com>
2023-06-23 10:59:30 +0700

    //jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR

--- CircularQueues_163/CircularQueues_163.cpp
+++ CircularQueues_163/CircularQueues_163.cpp
@@ -69,5 +69,15 @@ public:
 			cout << "Queue is empty\n ";
 			return;
 		}
+
+		cout << "\nelements in the queue are...\n";
+
+		//jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << " ";
+				FRONT_position++;
+			}
+		}
 	}
 };
