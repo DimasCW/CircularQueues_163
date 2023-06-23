59016d92d8ead211308cb396bdd371f2af200914
DimasCW <cdimas292@gmail.com>
2023-06-23 09:32:43 +0700

    menyelesaiskan class queues

--- CircularQueues_163/CircularQueues_163.cpp
+++ CircularQueues_163/CircularQueues_163.cpp
@@ -30,8 +30,12 @@ public:
 		}
 		else {
 			//jika REAR berada diposisi terakhir array, kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
 		}
-
+		queue_array[REAR] = num;
 	}
 
 };
