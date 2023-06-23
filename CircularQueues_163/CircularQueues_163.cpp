69a607d6f762ba4f584051080447a95ec4685d0e
DimasCW <cdimas292@gmail.com>
2023-06-23 11:16:21 +0700

    case 1

--- CircularQueues_163/CircularQueues_163.cpp
+++ CircularQueues_163/CircularQueues_163.cpp
@@ -103,6 +103,15 @@ int main() {
 			cout << "3. Display values" << endl;
 			cout << "4. Exit" << endl;
 			cout << "Enter your choice (1-4): ";
+			cin >> ch;
+			cout << endl;
+
+			switch (ch) {
+			case'1': {
+				q.insert();
+			}
+			}
+
 		}
 	}
 }
